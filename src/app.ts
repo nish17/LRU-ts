aeebdfa67b488deefe09301dd4b85821877636b8
Nimesh <nimesh3536@gmail.com>
2022-07-09 22:22:53 +0530

    works with inmemory example - add some test cases and comments

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,19 @@ export class Node {
   value: any;
   next: Node | null;
   prev: Node | null;
-  constructor(key: string, value: any, next = null, prev = null) {
+  constructor(
+    key: string,
+    value: any,
+    next: Node | null = null,
+    prev: Node | null = null
+  ) {
     this.key = key;
     this.value = value;
     this.next = next;
     this.prev = prev;
   }
 }
-
+// in-memory example
 export class LRU {
   private map: Map<string, any>;
   private maxItems: number = 5;
@@ -32,16 +37,67 @@ export class LRU {
     }
     const requiredNode = this.map.get(key);
     if (this.head !== requiredNode) {
-      this.set(key, requiredNode.value);
+      this.set(key, requiredNode!.value);
     }
-    return requiredNode.value;
+    return requiredNode!.value;
   }
   public set(key: string, value: any) {
-    // remove the node if running out of capacity
-    // remove if node exists (duplicate value) and write at the head
-    // add Node to cache
+    if (this.map.has(key)) {
+      this.removeNode(this.map.get(key));
+      this.currentSize--;
+    } else if (this.currentSize === this.maxItems) {
+      this.map.delete(this.tail!.key); // remove LRU as it is at tail
+      this.removeNode(this.tail!); // remove tail
+      this.currentSize--;
+    }
+    if (!this.head) {
+      // inserting first node
+      const newNode = new Node(key, value);
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      const newNode = new Node(key, value, this.head);
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.map.set(key, this.head);
+    this.currentSize++;
   }
-  public remove(node: Node) {
 
+  private removeNode(node: Node) {
+    if (node.prev) {
+      node.prev.next = node.next;
+    } else {
+      this.head = node.next;
+    }
+    if (node.next) {
+      node.next.prev = node.prev;
+    } else {
+      this.tail = node.prev;
+    }
   }
 }
+
+const cache = new LRU(3);
+// Test Case 1:
+cache.set("a", {});
+cache.set("b", {});
+cache.set("c", {});
+cache.set("d", {});
+// Result: 
+//  b   | c |  d
+// LRU  |   | MRU
+// tail |   | head
+
+// Test Case 2:
+cache.get("a");
+// Result: Not Present
+
+cache.get("b");
+cache.set("d", {});
+cache.set("c", {});
+// Result: 
+//  b   | d |  c
+// LRU  |   | MRU
+// tail |   | head
+console.log(cache);
